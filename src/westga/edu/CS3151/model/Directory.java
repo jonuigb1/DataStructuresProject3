1ba5008b2ca5e7bbadaf5d4f41a8627e857b16d4
jonuigb1 <jonuigb1@my.westga.edu>
2018-02-19 10:45:38 -0500

    have problems with the directory all i have to do is change
    the names for the methods to match JavaFx

---
+++ src/westga/edu/CS3151/model/Directory.java
@@ -0,0 +1,78 @@
+package westga.edu.CS3151.model;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javafx.stage.FileChooser;
+
+public class Directory {
+	private static ArrayList<String> list;
+
+	private static void addFiles(File dir) {
+		if (dir.isDirectory()) {
+			for (File file : dir.listFiles()) {
+				list.add(file.getAbsolutePath());
+				if (file.isDirectory()) {
+					addFiles(file);
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+
+		list = new ArrayList<String>();
+		FileChooser chooser = new FileChooser();
+		chooser.setFileSelectionMode(FileChooser.DIRECTORIES_ONLY);
+		chooser.setFileSelectionMode(FileChooser.FILES_ONLY);
+		chooser.setFileSelectionMode(FileChooser.FILES_AND_DIRECTORIES);
+		chooser.setAcceptAllFileFilterUsed(false);
+		int value = chooser.showOpenDialog(null);
+		if (value != FileChooser.APPROVE_OPTION) {
+			return;
+		}
+		addFiles(chooser.getSelectedFile());
+		for (String name : list) {
+			System.out.println(name);
+		}
+
+	}
+	
+	 public void search(String name,File file)
+	    {
+	        File[] list = file.listFiles();
+	        if(list!=null)
+	        for (File files : list)
+	        {
+	            if (files.isDirectory())
+	            {
+	                search(name,files);
+	            }
+	            else if (name.equalsIgnoreCase(files.getName()))
+	            {
+	                System.out.println(files.getParentFile());
+	            }
+	        }
+	    }
+	    public static void mains(String[] args) {
+	    	
+         Directory ff = new Directory();
+	        Scanner scan = new Scanner(System.in);
+	        System.out.println("Enter the file to be searched.. " );
+	        String name = scan.next();
+	        System.out.println("Enter the directory where to search ");
+	        String directory = scan.next();
+	        ff.search(name,new File(directory));
+	    }
+	
+
+
+	
+
+
+	
+	
+	
+
+}
